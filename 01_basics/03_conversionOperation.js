6b604d5a9d227794a8b9652c8fb92a61130ae667
MuhaddisRehman <muhaddisrehman12@gmail.com>
2026-02-23 21:23:56 +0500

    some more operations

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,3 +1,9 @@
 let someNumber = 33;
 let stringNumber = String(someNumber);
 console.log("🚀 ~ someNumber:", typeof someNumber);
+
+num1 = num2 = num3 = 2 + 2;
+
+let gameCounter = 100;
+++gameCounter;
+console.log(gameCounter);
